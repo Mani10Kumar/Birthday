b93fbb912a7e288d1037907374404e3951f185e9
Mani Kumar <52690994+Mani10Kumar@users.noreply.github.com>
2021-07-07 01:34:16 +0530

    Add files via upload

---
+++ dhoni.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <dos.h>
+using namespace std;
+
+int main()
+{
+	char h = '\3';
+	cout << endl << endl << endl << endl << endl << endl << "\t\t\t" << h << " Happy Birthday Wishing " << endl << "\t\t\tUsing C++ Language to Ms Dhoni " << h << endl << endl << endl;
+	Sleep(1002);
+	Beep(523,500);
+	system("cls");
+	cout << endl << endl << endl << "\t\t\tFrom: \t  CodeWithMani" << endl << "\t\t\tTo:\t Name:-MS Dhoni" << endl << "\t\t";
+	Sleep(500);
+
+
+	for (int i = 0; i < 40; i++) {
+		cout << "_";
+	}
+	Sleep(500);
+	Beep(523, 500);
+
+	cout << endl << endl << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t__i_i_i_i_i__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t       |" << h << "\t    " << h << "|__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t     __| " << h << "  7/7/2021 " << h << " |__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t    | " << h << "    " << h << "     " << h << "     " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t  __|" << h << "   HAPPY   " << h << "     " << h << "|__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "     " << h << "   BIRTHDAY    " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |" << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "  " << h << "    MS Dhoni  " << h << "     " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |";
+	for (int i = 0; i < 25; i++) {
+		cout << "_";
+	}
+	cout << "|" << endl<<endl<<endl;
+
+	cout << "\t\tHAPPY BIRTHDAY";
+	cout << endl << "\t\t\t\tMS Dhoni";
+	cout << endl << "\t\t\t\t\tFROM";
+	cout << endl << "\t\t\t\t|||\t |||";
+	cout << endl << "\t\t\t\tMani kumar";
+
+	
+}
